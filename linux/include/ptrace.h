c61d1b7d487d53e694df4b132fa85ed174883a24
0xvpr <malikrb@live.com>
2021-10-04 23:40:40 -0500

    lots

---
+++ linux/include/ptrace.h
@@ -0,0 +1,15 @@
+#ifndef _PTRACE_H
+#define _PTRACE_H
+
+void ptrace_attach(pid_t target);
+void ptrace_detach(pid_t target);
+void ptrace_getregs(pid_t target, struct REG_TYPE* regs);
+void ptrace_cont(pid_t target);
+void ptrace_setregs(pid_t target, struct REG_TYPE* regs);
+siginfo_t ptrace_getsiginfo(pid_t target);
+void ptrace_read(int pid, unsigned long addr, void *vptr, int len);
+void ptrace_write(int pid, unsigned long addr, void *vptr, int len);
+void checktargetsig(int pid);
+void restoreStateAndDetach(pid_t target, unsigned long addr, void* backup, int datasize, struct REG_TYPE oldregs);
+
+#endif /* _PTRACE_H */
--- linux/src/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    if (printf("Linux Payload Injector\n"))
-    {
-    }
-}

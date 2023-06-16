540840928bf38556860eec2ddcb751d486e25bbc
VPR <malikrb@live.com>
2023-06-16 19:23:15 -0400

    README changes; Breakpoint logic removed from detour once

--- src/pidjeon/include/logger.h
+++ src/pidjeon/include/logger.h
@@ -4,8 +4,6 @@
 #include "definitions.h"
 #include <inttypes.h>
 
-char* InsertSpacing(unsigned shiftwidth, char* buffer, size_t size);
-
 int LogEvent(Injector* injector, const char* restrict event, unsigned shiftwidth);
 
 #endif /* LOGGER_HEADER */

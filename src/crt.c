4929632042553a0d70510466b32b32e76dd2e689
VPR <>
2024-03-22 22:23:23 -0400

    Make python GUI mvp

--- src/crt.c
+++ src/crt.c
@@ -32,7 +32,7 @@ __forceinline char* extract_bytes_from_file(const char* const restrict f, size_t
 unsigned create_remote_thread(Resource* const restrict resource, Injector* const restrict injector)
 {
     exec_mem_t              exec_mem = NULL;
-    HANDLE                  hProcess = NULL;
+    HANDLE                  process_handle = NULL;
     HANDLE                  hThread = NULL;
     DWORD                   old_protect = 0;
     BOOL                    bSuccess = 0;
@@ -40,55 +40,69 @@ unsigned create_remote_thread(Resource* const restrict resource, Injector* const
     size_t                  size = 0;
 
     LOG_MSG(injector, "Opening target process..", 0);
-    if (!(hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, resource->process_id)))
+    if (!(process_handle = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, resource->process_id)))
     {
         LOG_MSG(injector, "Failed to acquire handle to target", 0);
-        CloseHandle(hProcess);
+        CloseHandle(process_handle);
         return 1;
     }
     LOG_MSG(injector, "Process Handle to target acquired", 0);
 
     LOG_MSG(injector, "Allocating virtual memory to target..", 0);
     payload = extract_bytes_from_file(resource->relative_payload_path, &size);
-    if (!(exec_mem = (exec_mem_t)VirtualAllocEx(hProcess, NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE)))
+    if (!(exec_mem = (exec_mem_t)VirtualAllocEx(process_handle, NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE)))
     {
-        printf("exec_mem: %p\n", exec_mem);
         LOG_MSG(injector, "Failed to allocate memory to target", 0);
-        CloseHandle(hProcess);
+        printf("exec_mem: %p\n", exec_mem);
+        CloseHandle(process_handle);
         return 2;
     }
     LOG_MSG(injector, "Allocation successful", 0);
 
     LOG_MSG(injector, "Writing payload to executable memory..", 0);
-    if (!(WriteProcessMemory(hProcess, (LPVOID)exec_mem, (LPVOID)payload, size, NULL)))
+    if (!(WriteProcessMemory(process_handle, (LPVOID)exec_mem, (LPVOID)payload, size, NULL)))
     {
-        free(payload);
         LOG_MSG(injector, "Failed to write payload to executable memory", 0);
+        free(payload);
+        CloseHandle(process_handle);
         return 3;
     }
     free(payload);
     LOG_MSG(injector, "Write success", 0);
 
     LOG_MSG(injector, "Changing virtual protection permissions..", 0);
-    if (!(bSuccess = VirtualProtectEx(hProcess, (LPVOID)exec_mem, size, PAGE_EXECUTE_READ, &old_protect)))
+    if (!(bSuccess = VirtualProtectEx(process_handle, (LPVOID)exec_mem, size, PAGE_EXECUTE_READ, &old_protect)))
     {
         LOG_MSG(injector, "Failed to change virtual protection permissions", 0);
+        VirtualFreeEx(process_handle, exec_mem, size, MEM_RELEASE);
+        CloseHandle(process_handle);
         return 4;
     }
     LOG_MSG(injector, "Protection changed", 0);
 
     LOG_MSG(injector, "Creating remote thread..", 0);
-    if (!(hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)exec_mem, NULL, 0, NULL)))
+    if (!(hThread = CreateRemoteThread(process_handle, NULL, 0, (LPTHREAD_START_ROUTINE)exec_mem, NULL, 0, NULL)))
     {
         LOG_MSG(injector, "Failed to create remote thread", 0);
+        VirtualFreeEx(process_handle, exec_mem, size, MEM_RELEASE);
+        CloseHandle(process_handle);
         return 5;
     }
     LOG_MSG(injector, "Remote thread created", 0);
     
-    // optional wait for object
+    // Allow injecto to specify if we wait for the thread to finish
     // WaitForSingleObject(hThread, INFINITE);
     
+    // LOG_MSG(injector, "Freeing allocated memory..", 0);
+    // if (!VirtualFreeEx(process_handle, exec_mem, size, MEM_RELEASE))
+    // {
+    //     LOG_MSG(injector, "Failed to free allocated memory", 0);
+    //     CloseHandle(process_handle);
+    //     return 6;
+    // }
+    // LOG_MSG(injector, "Allocated memory released", 0);
+    
     LOG_MSG(injector, "Closing handle and returning control to owner..", 0);
-    CloseHandle(hProcess);
+    CloseHandle(process_handle);
     return 0;
 }
--- vpr-pidjeon/__main__.py
+++ vpr-pidjeon/__main__.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3.exe
 
+from .types import (
+    Resource,
+    Injector,
+)
+
+from .c_bindings import (
+    create_remote_thread,
+    log_basic
+)
+
 import platform
 import ctypes
 import psutil
@@ -7,48 +17,183 @@ import sys
 
 from argparse import ArgumentParser, Namespace
 
+from PyQt5.QtCore import Qt
+
 from PyQt5.QtWidgets import (
     QApplication,
+    QMenu,
     QMainWindow,
+    QPushButton,
     QTableWidget,
     QTableWidgetItem,
+    QHBoxLayout,
     QVBoxLayout,
     QWidget,
+    QLineEdit,
+    QFileDialog,
+    QGroupBox
 )
-from PyQt5.QtCore import QTimer
 
-class TaskManager(QMainWindow):
+class GUI(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
 
+        self.initUI()
+        self.tableWidget.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.tableWidget.customContextMenuRequested.connect(self.contextMenu)
+    
     def initUI(self):
-        self.setWindowTitle('Real-Time Task Manager')
+        self.setWindowTitle('vpr-pidjeon')
         self.setGeometry(100, 100, 400, 600)
+        self.setMinimumSize(600, 400)
+
+
 
         self.tableWidget = QTableWidget()
         self.tableWidget.setColumnCount(2)
         self.tableWidget.setHorizontalHeaderLabels(['Process Name', 'PID'])
+        self.tableWidget.setSortingEnabled(True)
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.tableWidget)
+        self.outer_layout = QVBoxLayout()
+        self.outer_layout.setContentsMargins(0, 0, 0, 0)
+        self.outer_layout.setSpacing(0)
+
+        self.payload_layout = QHBoxLayout()
+        self.payload_layout.setContentsMargins(0, 0, 0, 0)
+        self.payload_layout.setSpacing(0)
+
+        process_box   = QGroupBox("Processes")
+        self.process_layout = QVBoxLayout()
+        self.process_layout.setContentsMargins(0, 0, 0, 0)
+        self.process_layout.setSpacing(0) 
+        process_box.setLayout(self.process_layout)
+
+        # Create the filter bar
+        self.filter = QLineEdit()
+        self.filter.setPlaceholderText("filter...")
+
+        self.refreshButton = QPushButton("Refresh")
+        self.refreshButton.clicked.connect(self.updateProcessList) 
+
+
+        # Layout box order
+        self.process_layout.addWidget(self.filter)
+        self.process_layout.addWidget(self.tableWidget)
+        self.process_layout.addWidget(self.refreshButton)
 
         self.container = QWidget()
-        self.container.setLayout(self.layout)
+        self.container.setLayout(self.outer_layout)
         self.setCentralWidget(self.container)
 
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.updateProcessList)
-        self.timer.start(1000)  # Refreshes every 1000 milliseconds
+        # Must happen first
+        self.updateProcessList()
+
+        # Sizing
+        row_height = self.tableWidget.rowHeight(0)
+        self.filter.setFixedHeight(row_height)
+        self.filter.setFixedWidth(400)
+        self.filter.setMinimumWidth(300)
+
+        self.refreshButton.setFixedHeight(row_height)
+        self.refreshButton.setFixedWidth(400)
+        self.refreshButton.setMinimumWidth(300)
+
+        self.tableWidget.setFixedHeight((row_height * 11) + 4)
+        self.tableWidget.setFixedWidth(400)
+        self.tableWidget.setMinimumWidth(300)
+        self.tableWidget.setMinimumHeight(200)
+
+        # Actions
+        self.tableWidget.cellDoubleClicked.connect(self.double_clicked_cell)
+
+        payload_box   = QGroupBox("Payloads")
+        payload_box.setLayout(self.payload_layout)
+
+        parameter_box = QGroupBox("Parameters")
+        parameter_layout = QHBoxLayout()
+        parameter_box.setLayout(parameter_layout)
+
+        self.targetBox = QLineEdit()
+        self.targetBox.isEnabled = False
+        self.targetBox.setPlaceholderText("Target...")
+        parameter_layout.addWidget(self.targetBox)
+
+        self.injectButton = QPushButton("Inject")
+        self.injectButton.clicked.connect(self.inject_clicked)
+        parameter_layout.addWidget(self.injectButton)
+
+        self.payloadBox = QLineEdit()
+        self.payloadBox.setPlaceholderText("Select payload...")
+        self.payload_layout.addWidget(self.payloadBox)
+
+        self.openFileDialogButton = QPushButton("Browse")
+        self.openFileDialogButton.clicked.connect(self.openFileDialog)
+        self.payload_layout.addWidget(self.openFileDialogButton)
+
+
+        self.outer_layout.addWidget(process_box)
+        self.outer_layout.addWidget(payload_box)
+        self.outer_layout.addWidget(parameter_box)
+
+    def inject_clicked(self):
+        target = self.targetBox.text()
+        payload = self.payloadBox.text()
+
+        resource = Resource(
+            process_id=self.process_id,
+            target_process=target.encode(),
+            relative_payload_path=payload.encode()
+        )
+        injector = Injector(
+            logger=log_basic
+        )
+
+        create_remote_thread(
+            ctypes.byref(resource),
+            ctypes.byref(injector)
+        )
+
+    def double_clicked_cell(self, row, col):
+        self.name = self.tableWidget.item(row, 0).text().encode()
+        self.process_id = ctypes.c_uint32( int(self.tableWidget.item(row, 1).text()) )
+
+        self.targetBox.setText(self.name.decode())
 
     def updateProcessList(self):
-        self.tableWidget.setRowCount(0)  # Clear the table
+        searchQuery = self.filter.text().lower()
+        self.tableWidget.setSortingEnabled(False) 
+        self.tableWidget.setRowCount(0)
         for process in psutil.process_iter(['pid', 'name']):
-            rowPosition = self.tableWidget.rowCount()
-            self.tableWidget.insertRow(rowPosition)
-            self.tableWidget.setItem(rowPosition, 0, QTableWidgetItem(process.info['name']))
-            self.tableWidget.setItem(rowPosition, 1, QTableWidgetItem(str(process.info['pid'])))
+            processName = process.info['name']
+            if searchQuery in processName.lower():
+                rowPosition = self.tableWidget.rowCount()
+                self.tableWidget.insertRow(rowPosition)
+                nameItem = QTableWidgetItem(processName)
+                pidItem = QTableWidgetItem(str(process.info['pid']))
+                
+                nameItem.setFlags(nameItem.flags() & ~Qt.ItemIsEditable)
+                pidItem.setFlags(pidItem.flags() & ~Qt.ItemIsEditable)
+                
+                self.tableWidget.setItem(rowPosition, 0, nameItem)
+                self.tableWidget.setItem(rowPosition, 1, pidItem)
+        self.tableWidget.setSortingEnabled(True)
+    
+    def contextMenu(self, position):
+        menu = QMenu()
+        copyAction = menu.addAction("Copy")
+        action = menu.exec_(self.tableWidget.viewport().mapToGlobal(position))
+        if action == copyAction:
+            selected = self.tableWidget.selectedItems()
+            if selected:
+                clipboard = QApplication.clipboard()
+                clipboard.setText(selected[0].text())
 
+    def openFileDialog(self):
+       options = QFileDialog.Options()
+       options |= QFileDialog.DontUseNativeDialog
+       fileName, _ = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*);;Text Files (*.txt)", options=options)
+       if fileName:
+           self.payloadBox.setText(fileName)
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
@@ -66,7 +211,7 @@ def parse_args() -> Namespace:
 
 def main() -> None:
     app = QApplication(sys.argv)
-    ex = TaskManager()
+    ex = GUI()
     ex.show()
     sys.exit(app.exec_())
 
